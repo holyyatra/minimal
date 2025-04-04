b473bd553771c1acd611314cce2cd86fff74aee5
holyyatra <guptaasish842@gmail.com>
2025-04-04 15:25:25 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-8import Image from "next/image"
+import Image from "next/image"
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
 import { Card } from "@/components/ui/card"
