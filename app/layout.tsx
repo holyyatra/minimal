1922abee328ae67715f2eb87d99c8f964065fd03
holyyatra <guptaasish842@gmail.com>
2025-04-04 12:55:56 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Holy Yatra',
+  description: 'Tour and Travels',
+  generator: 'holyatra',
 }
 
 export default function RootLayout({
